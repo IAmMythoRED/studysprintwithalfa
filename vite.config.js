80f9533134afe6b79ce927f4938fa1268daeb682
unknown <botbuddieslist@gmail.com>
2025-01-22 17:28:32 +0530

    update: vite

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,5 @@ import commonjs from 'vite-plugin-commonjs'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),commonjs()],
-  base: "/studysprintwithalfa/"
+  base: "/studysprintwithalfa"
 })
